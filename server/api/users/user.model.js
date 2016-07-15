3e6594300a76fb33fa4b9432cd0c66b602b6c514
Ayako Mikami <ayakomikami11@gmail.com>
2016-07-15 16:26:20 -0400

    encrypted

---
+++ server/api/users/user.model.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+
+var db = require('../../_db');
+var crypto = require('crypto');
+var iterations = 1;
+var bytes = 64;
+
+var User = db.define('user', {
+  name: Sequelize.STRING,
+  photo: {
+    type: Sequelize.STRING,
+    defaultValue: '/images/default-photo.jpg'
+  },
+  phone: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type:Sequelize.STRING,
+    set: function(val){
+      var salt = crypto.randomBytes(16).toString('base64');
+      var buffer = crypto.pbkdf2Sync(val, salt, iterations, bytes);
+      var hash = buffer.toString('base64');
+      this.setDataValue('password', hash);
+      this.setDataValue('salt', salt);
+    }
+  },
+  salt: {
+    type: Sequelize.STRING,
+    get: function(){
+      return this.getDataValue('salt');
+    }
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
+  googleId: Sequelize.STRING,
+  twitterId: Sequelize.STRING,
+  githubId: Sequelize.STRING
+});
+
+module.exports = User;
---
+++ server/auth/auth.router.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var router = require('express').Router();
+
+var HttpError = require('../utils/HttpError');
+var User = require('../api/users/user.model');
+var crypto = require('crypto');
+var iterations = 1;
+var bytes = 64;
+
+router.post('/login', function (req, res, next) {
+  User.findOne({
+    where: req.body
+  })
+  .then(function (user) {
+    if (!user) throw HttpError(401);
+    req.login(user, function (err) {
+      if (err) next(err);
+      else res.json(user);
+    });
+  })
+  .catch(next);
+});
+
+router.post('/signup', function (req, res, next) {
+  var salt = crypto.randomBytes(16).toString('base64');
+  var buffer = crypto.pbkdf2Sync(req.body.password, salt, iterations, bytes);
+  var hash = buffer.toString('base64');
+  console.log("HASH",hash);
+  console.log("REQBODY", req.body);
+  req.body.password = hash;
+  User.create(req.body)
+  .then(function (user) {
+    req.login(user, function (err) {
+      if (err) next(err);
+      else res.status(201).json(user);
+    });
+  })
+  .catch(next);
+});
+
+router.get('/me', function (req, res, next) {
+  res.json(req.user);
+});
+
+router.delete('/me', function (req, res, next) {
+  req.logout();
+  res.status(204).end();
+});
+
+router.use('/google', require('./google.oauth'));
+
+router.use('/twitter', require('./twitter.oauth'));
+
+router.use('/github', require('./github.oauth'));
+
+module.exports = router;
